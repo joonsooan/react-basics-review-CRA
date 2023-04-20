fa1c2d9bf2210fc52748f15e21cfef45e130b49c
joonsooan <joonsooan@gmail.com>
2023-04-20 10:45:51 +0900

    To do list

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import { useState, useEffect } from "react";
 
-function Hello() {
-  useEffect(() => {
-    console.log("Created :)");
-    return () => console.log("Destroyed :(");
-  }, []);
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  const [show, setShow] = useState(false);
-  const onClick = () => setShow((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return; // toDo가 비어있는 경우 submit을 수행하지 않음.
+    }
+    setToDos((currentArray) => [...currentArray, toDo]);
+    setToDo(""); // submit 이후 input을 비워줌.
+  };
+  console.log(toDos);
   return (
     <div>
-      {show ? <Hello /> : null}
-      <button onClick={onClick}>{show ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do.."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
