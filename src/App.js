aa490e6599f2628870d31f2fa9fd85ae57952cbb
joonsooan <joonsooan@gmail.com>
2023-05-01 16:47:28 +0900

    Finished reviewing!

--- src/App.js
+++ src/App.js
@@ -1,49 +1,22 @@
-import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [coinPrice, setCoinPrice] = useState(1);
-  const [money, setMoney] = useState(0);
-  useEffect(() => {
-    fetch(`https://api.coinpaprika.com/v1/tickers`)
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      }); // 네트워크에서 JSON 파일을 가져와서 coins에 저장.
-  }, []);
-  const handleCoinPrice = (event) => {
-    setCoinPrice(event.target.value);
-  };
-  const handleMoneyInput = (event) => {
-    setMoney(event.target.value);
-  };
   return (
-    <div>
-      <h1>
-        {loading ? "Loading..." : `We have ${coins.length} types of Coins`}
-      </h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <div>
-          <select onChange={handleCoinPrice}>
-            <option>Select a Coin!</option>
-            {coins.map((coin, index) => (
-              <option key={index} value={coin.quotes.USD.price}>
-                {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
-              </option>
-            ))}
-          </select>
-          <div>
-            <h3>How much money do you have?</h3>
-            <input type="number" value={money} onChange={handleMoneyInput} />$
-          </div>
-          <h3>You can have {money / coinPrice} coins</h3>
-        </div>
-      )}
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/hello">
+          <h1>Hello</h1>
+        </Route>
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
